d23a94b0184b863c3177d3913499442ec8b4b93b
Joanie522 <joanietwersky@Christynas-MacBook-Air.local>
2016-07-20 17:22:30 -0400

    first commit

---
+++ main_program.rb
@@ -0,0 +1,49 @@
+puts "hello"
+
+
+# class Main
+# 	def initialize(zip_code, pick_up_date, return_date, promo_code, renter_age)
+# 		@zip_code = zip_code
+# 		@pick_up_date = pick_up_date
+# 		@return_date = return_date
+# 		@promo_code = promo_code
+# 		@renter_age = renter_age
+# 	end
+
+# 	def zip_code
+# 		@zip_code
+# 	end
+
+# 	def pick_up_date
+# 		@pick_up_date
+# 	end
+
+# 	def return_date
+# 		@return_date
+# 	end
+
+# 	def promo_code
+# 		@promo_code
+# 	end
+
+# 	def renter_age
+# 		@renter_age
+# 	end
+
+
+
+
+
+# puts "Start a reservation:"
+# 	print "Zip: "
+# 	zip_code = gets.chomp
+# 	print "Pick-up Date: "
+# 	pick_up_date = gets.chomp
+# 	print "Return-date "
+# 	return_date = gets.chomp
+# 	print "Promo code: "
+# 	promo_code = gets.chomp
+# 	print "Renter Age: "
+# 	renter_age = gets.chomp
+
+# end

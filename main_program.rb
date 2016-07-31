468c4c004bcc3fde57dc860a16a71a404a55058f
Joanie522 <joanietwersky@unknown8438355f39ae.attlocal.net>
2016-07-31 19:59:21 -0400

    fixed menu method

--- main_program.rb
+++ main_program.rb
@@ -1,53 +1,39 @@
-# class Options
-# 	def initialize(Option_1, Option_2, Option_3, Option_4)
-# 		@Option_1 = Option_1
-# 		@Option_2 = Option_2
-# 		@Option_3 = Option_3
-# 		@Option_4 = Option_4
-# 	end
+def display_menu clear, *msg
+	if clear == true
+		system "clear"
+	end
 
-# 	def Option_1
-# 		@Option_1
-# 	end
+	puts "*************************************************"
+	puts "****  Welcome to the Car Rental Application  ****"
+	puts "*************************************************"
+	puts "****  Please select from the following menu: ****"
+	puts "**** 1. Option 1"
+	puts "**** 2. Option 2"
+	puts "**** 3. Option 3"
+	puts "**** 4. Option 4"
 
-# 	def Option_2
-# 		@Option_2
-# 	end
+	if !msg.empty?
+		print_message(msg)
+	end
 
-# 	def Option_3
-# 		@Option_3
-# 	end
+	answer = gets.chomp.to_i
 
-# 	def Option_4
-# 		@Option_4
-# 	end
-
-# 	def display_menu
-# 	@display_menu = display_menu
-# 	end
-
-# end
-
-# selections = Options.new("Customer Details", "Vehicle Details", "Rent A Car", "Assign Vehicles" )
-
-puts "*************************************************
-****  Welcome to the Car Rental Application  ****
-*************************************************
-****  Please select from the following menu: ****
-**** 1. Option 1
-**** 2. Option 2
-**** 3. Option 3
-**** 4. Option 4"
-
-answer = gets.chomp.to_i
-if answer == 1
-    puts "You chose Option 1"
-elsif answer == 2
-	puts "You chose Option 2"
-elsif answer == 3
-	puts "Your chose Option 3"
-elsif answer == 4
-	puts "You chose Option 4"
-else
-	puts "Not a valid Option"
+	if answer == 1
+	    puts "You chose Option 1"
+	elsif answer == 2
+		puts "You chose Option 2"
+	elsif answer == 3
+		puts "Your chose Option 3"
+	elsif answer == 4
+		puts "You chose Option 4"
+	else
+		puts "Not a valid Option"
+		display_menu(true)
+	end
 end
+
+def print_message msg
+	puts "!!! #{msg} !!!!"
+end
+
+display_menu(true)
